7d30cff6859845a513a2e4d839acc2ac542e6d2b
Lucas Nunes <lucasnunnes40@gmail.com>
2024-10-11 21:39:27 -0300

    criando e iniciando o código fonte

---
+++ codigo_fonte/chamaAi/src/main/java/com/inf/ufg/chamaAi/ChamaAiApplication.java
@@ -0,0 +1,13 @@
+package com.inf.ufg.chamaAi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChamaAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChamaAiApplication.class, args);
+	}
+
+}
---
+++ codigo_fonte/chamaAi/src/test/java/com/inf/ufg/chamaAi/ChamaAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inf.ufg.chamaAi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChamaAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
